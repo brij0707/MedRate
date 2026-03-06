8104bdb5c41b912be62e31f9552a6d9c474f71bd
Brij Nandan Tripathi <52787434+brij0707@users.noreply.github.com>
2026-03-06 10:16:05 +0530

    Delete mobile_app/lib/config.dart

--- mobile_app/lib/config.dart
+++
@@ -1,6 +0,0 @@
-class AppConfig {
-  static const String geminiApiKey = 'YOUR_ACTUAL_API_KEY_HERE';
-  static const String supabaseUrl = 'YOUR_SUPABASE_URL';
-  static const String supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
-}
-
